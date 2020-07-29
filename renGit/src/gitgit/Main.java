8aad30e70b81ae71d5520535771351f9bdf39d62
Akira-Inoue0219 <aip.mtgw@gmail.com>
2020-07-29 09:45:12 +0900

    新規プロジェクト作成。

---
+++ renGit/src/gitgit/Main.java
@@ -0,0 +1,19 @@
+package gitgit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+
+		System.out.println("数を入力");
+		int a = new java.util.Scanner(System.in).nextInt();
+		int r =new java.util.Random().nextInt(4);
+		if(a==r) {
+			System.out.println("あたり");
+		}else {
+			System.out.println("はずれ");
+		}
+
+	}
+
+}
